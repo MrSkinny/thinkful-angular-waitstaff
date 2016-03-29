955053526094fea6b4a7d74cc736a63a6fd0b625
Rich Greenhill <rgnet1@yahoo.com>
2016-03-29 13:25:48 -0700

    auto change calculations

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-angular.module('waitStaff', []);
+angular.module('waitStaff', [])
+
+  .controller('MainCtrl', function($scope){
+    var main = this;
+    
+    main.inputs = {};
+    main.inputs.baseMealPrice = 0;
+    main.inputs.taxRate = 0;
+    main.inputs.tipPercentage = 0;
+    
+    $scope.$watchCollection('main.inputs', function(newVal,oldVal){
+      console.log(newVal, oldVal);
+      calcChargesFromInputs();
+    });
+    
+    function calcChargesFromInputs(){
+      // calculate subtotal
+      main.customerSubtotal = main.inputs.baseMealPrice * ( (main.inputs.taxRate / 100) + 1 );
+
+      // calculate tip
+      main.customerTip = main.customerSubtotal * ( (main.inputs.tipPercentage / 100) );
+
+      // calculate total
+      main.customerTotal = main.customerSubtotal + main.customerTip;
+    }
+    
+    main.calculate = function(){
+      
+      console.log(main.inputs.baseMealPrice, main.inputs.taxRate, main.inputs.tipPercentage);
+
+      // reset input values
+      main.inputs.baseMealPrice = 0;
+      main.inputs.taxRate = 0;
+      main.inputs.tipPercentage = 0;
+      
+    };
+  });
