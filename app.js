239f80a352a96727a35f287c0c29014da19c839c
Rich Greenhill <rgnet1@yahoo.com>
2016-03-29 13:29:48 -0700

    implement cancel button; re-org code

--- app.js
+++ app.js
@@ -3,16 +3,23 @@ angular.module('waitStaff', [])
   .controller('MainCtrl', function($scope){
     var main = this;
     
+    // Set Initial Values
     main.inputs = {};
     main.inputs.baseMealPrice = 0;
     main.inputs.taxRate = 0;
     main.inputs.tipPercentage = 0;
     
+    // Listener on value changes
     $scope.$watchCollection('main.inputs', function(newVal,oldVal){
       console.log(newVal, oldVal);
       calcChargesFromInputs();
     });
     
+    // Buttons
+    main.calculate = triggerCalculateButton;
+    main.cancel = triggerCancelButton;
+    
+    // Functions
     function calcChargesFromInputs(){
       // calculate subtotal
       main.customerSubtotal = main.inputs.baseMealPrice * ( (main.inputs.taxRate / 100) + 1 );
@@ -24,14 +31,20 @@ angular.module('waitStaff', [])
       main.customerTotal = main.customerSubtotal + main.customerTip;
     }
     
-    main.calculate = function(){
-      
+    function triggerCalculateButton(){
       console.log(main.inputs.baseMealPrice, main.inputs.taxRate, main.inputs.tipPercentage);
 
       // reset input values
       main.inputs.baseMealPrice = 0;
       main.inputs.taxRate = 0;
       main.inputs.tipPercentage = 0;
-      
-    };
+    }
+    
+    function triggerCancelButton(){
+      // reset input values
+      main.inputs.baseMealPrice = 0;
+      main.inputs.taxRate = 0;
+      main.inputs.tipPercentage = 0;
+    }
+    
   });
